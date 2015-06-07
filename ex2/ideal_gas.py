62b522052144ee7a2f1551e27cbdaaf5ce628d10
Will Haines <w@willhaines.me>
2015-06-07 09:50:20 -0600

    Added ideal gas law skeleton

---
+++ ex2/ideal_gas.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+from numpy import *
+
+def ideal_gas(in_array):
+    """Solve the ideal gas law PV=nRT
+
+    in_array -- [P,V,n,T] Set one value to None and this function will
+                solve for that term.
+    returns  -- a single number corresponding to omitted input value
+    """
+    print(x)
+    return 0
+
+
+
+x = array([1.3,2,None,35])
+ideal_gas(x)
